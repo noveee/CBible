847e1dba52bb8a1b597aae4134fe698eb5486153
noveee <thepythonmc@gmail.com>
2025-07-31 13:39:28 -0400

    beginner new section

--- test.c
+++ test.c
@@ -1,13 +1,28 @@
 #include <stdio.h> 
 
 int main(){
-    int count, newline;
-    newline = 0;
+    int c, i, nwhite, nother;
+    int ndigit[10];
 
-    while ((count = getchar()) != EOF){
-        if (count == '\n'){
-            ++newline;
+    nwhite = nother = 0;
+
+    for (i = 0; i < 10; ++i){
+        ndigit[i] = 0;
+    }
+
+    while ((c = getchar()) != EOF){
+        if (c >= '0' && c <= '9'){
+            ++ndigit[c-'0'];
+        } else if (c == ' ' || c == '\n' || c == '\t'){
+            ++nwhite;
+        } else {
+            ++nother;
         }
     }
-    printf("%d\n", newline);
+    printf("digits = ");
+    for (i = 0; i < 10; ++i){
+        printf(" %d", ndigit[i]);
+    }
+
+    printf(", white space = %d, other = %d\n", nwhite, nother);
 }
